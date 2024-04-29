440ee02cfe7802f2b3aadd73dc1216f3c807a25e
Houlbergg <s164578@dtu.dk>
2024-04-29 02:29:57 +0200

    Commit test

---
+++ Python/FlowCalcs.py
@@ -0,0 +1,34 @@
+# Using the formula v_e = flow_factor * ( Q / A_e )
+# A_e = w_e * h_e * epsilon
+# where epsilon is the porosity of the electrode.
+# This effectively reduces the area of the electrode face by "removing" the area not available to the flow.
+
+Q_old = [5, 10, 20, 50]  # Volumetric flow rate in mL/min
+v_e = [0.5, 1, 2, 5]  # Flow velocities from Baichen in cm/s
+w_e = [2.3]  # Width of electrode profile in cm
+h_e = [0.175]  # Height of electrode profile in cm
+epsilon = [0.93]  # Porosity of the electrode.
+flow_factor = [7, 8, 10]
+# Referenced value based on the following article.
+# Should be recalculated for each electrode
+# https://www-sciencedirect-com.proxy.findit.cvt.dk/science/article/pii/S037877531301567X
+# Flow factor is somewhat arbitrary. But based on the two following articles should be somewhere between 7 and 10
+# https://www-sciencedirect-com.proxy.findit.cvt.dk/science/article/pii/S037877531301567X
+# https://www.sciencedirect.com/science/article/pii/S2405896322004189
+
+# Calculate the new flow rate.
+# Q = ( v_e * A_e ) / flow_factor
+# Q = v_e * (w_e * h_e * epsilon) / flow_factor
+# Unit corrections. w_e, h_e, v_e / 100 to go from cm to m
+# Q * 1000000 to go from m^3/s to mL/s
+# Q * 60 to go from mL/s to mL/min
+
+Qs = {}
+
+for flow in flow_factor:
+    Q_s = []
+    for v in v_e:
+        Q_s.append(
+            (60000000 * v / 100 * w_e[0] / 100 * h_e[0] / 100 * epsilon[0]) / flow
+        )
+    Qs[flow] = Q_s
