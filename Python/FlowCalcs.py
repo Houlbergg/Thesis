629245580ed5449e152d60550bc15410e7311341
Houlbergg <s164578@dtu.dk>
2024-04-30 08:13:21 +0200

    Commit test

--- Python/FlowCalcs.py
+++ Python/FlowCalcs.py
@@ -2,8 +2,7 @@
 # A_e = w_e * h_e * epsilon
 # where epsilon is the porosity of the electrode.
 # This effectively reduces the area of the electrode face by "removing" the area not available to the flow.
-
-Q_old = [5, 10, 20, 50]  # Volumetric flow rate in mL/min
+electrodes = ["Felt", "Cloth", "Paper"]  # Electrodes
 Q_Baichen = [
     67.5,
     34.4,
@@ -35,17 +34,20 @@ CRs = [0.3, 0.2, 0.2]  # Compression ratios for Felt, Cloth, Paper
 # Q = v_e * (w_e * h_e * epsilon) / flow_factor
 
 # Unit corrections. w_e, h_e, v_e / 100 to go from cm to m
-# Q * 1000000 to go from m^3/s to mL/s
+# Q * 1 000 000 to go from m^3/s to mL/s
 # Q * 60 to go from mL/s to mL/min
-
+# Q * 60 000 to go from L/s to mL/min
 Q_CR0 = {}
 Q_CRs = {}
 
-for v in v_e:
-    for cr in CRs:
-        Q_CR0[v] = 60 * v * w_e[0] * h_e[0]
-        Q_CRs[v] = 60 * v * w_e[0] * h_e[0] * (1 - cr)
-
+for i, elec in enumerate(electrodes):
+    Q0s = []
+    Qs = []
+    for j, v in enumerate(v_e):
+        Q0s.append(60 * v * w_e[0] * h_e[i])
+        Qs.append(60 * v * w_e[0] * h_e[i] * (1 - CRs[i]))
+    Q_CR0[elec] = Q0s
+    Q_CRs[elec] = Qs
 Q_expanded = {}
 
 for flow in flow_factor:
@@ -53,3 +55,23 @@ for flow in flow_factor:
     for v in v_e:
         Q_s.append((60 * v * w_e[0] * h_e[0] * epsilon[0]) / flow)
     Q_expanded[flow] = Q_s
+
+A_geo = 2.3**2  # Area of the electrode face in cm^2
+current_density = 75  # mA/cm^2
+current = current_density * A_geo  # Current in mA
+n = 1  # Number of electrons
+F = 96485  # Faraday constant
+
+
+def k_m(v):  #
+    return 1.6 * 10 ** (-4) * v**0.4
+
+
+C_b = 1  # Molar concentration of the electrolyte in mol/L
+Q_old = [5, 10, 20, 50]  # Volumetric flow rate in mL/min
+i_L = []
+
+for vv in v_e:
+    i_L.append(n * F * k_m(vv))
+
+Q_naive = ((current / 1000) / (n * F * C_b)) * 60000  # Naive flow rate in mL/min
